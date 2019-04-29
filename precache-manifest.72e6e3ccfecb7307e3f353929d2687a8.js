c94211fda69ffd8439b2514064b667b3a900e1a1
SelinaXie <selinaxiem@gmail.com>
2019-04-29 11:50:04 +0800

    Updates

---
+++ precache-manifest.72e6e3ccfecb7307e3f353929d2687a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d43f8dd86067aa0dbf53074e0f20049",
+    "url": "/Laundrycats/index.html"
+  },
+  {
+    "revision": "1cf953e89c4d9a9ec0fa",
+    "url": "/Laundrycats/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/Laundrycats/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "1cf953e89c4d9a9ec0fa",
+    "url": "/Laundrycats/static/js/main.4855b874.chunk.js"
+  },
+  {
+    "revision": "334a6ad2e229cc524211",
+    "url": "/Laundrycats/static/js/runtime~main.03b03b46.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Laundrycats/static/media/logo.5d5d9eef.svg"
+  }
+]);
